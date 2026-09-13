bc164a3cc37d5b12039c4b1642a684be01a82fb2
puskarwagle <puskarwagle17@gmail..com>
2026-09-13 09:30:55 +0545

    [lib] Use Unsplash faces for 10 sample customers

--- src/lib/seedCustomers.js
+++ src/lib/seedCustomers.js
@@ -1,17 +1,25 @@
+const face = (id) =>
+  `https://images.unsplash.com/${id}?ixlib=rb-4.1.0&w=200&q=80&fm=jpg&crop=faces&fit=crop`;
+
+const FACES = {
+  manBlackCrew: face('photo-1624395213043-fa2e123b2656'),
+  manTurban: face('photo-1493106819501-66d381c466f1'),
+  manBlueRedShirt: face('photo-1545167622-3a6ac756afa4'),
+  womanSmiling: face('photo-1494790108377-be9c29b29330'),
+  manBlackCrew2: face('photo-1587397845856-e6cf49176c70'),
+  womanBlackCrew: face('photo-1534528741775-53994a69daeb'),
+  manHenley: face('photo-1500648767791-00dcc994a43e'),
+};
+
 export const SEED_CUSTOMERS = [
-  { name: 'Ram Bahadur Thapa', due: 1250 },
-  { name: 'Sita Sharma', due: 0 },
-  { name: 'Hari Prasad Adhikari', due: 340 },
-  { name: 'Gita Gurung', due: 0 },
-  { name: 'Krishna Yadav', due: 2100 },
-  { name: 'Laxmi Magar', due: 150 },
-  { name: 'Dilli Ram Pokharel', due: 0 },
-  { name: 'Mina Tamang', due: 875 },
-  { name: 'Bishnu Shrestha', due: 0 },
-  { name: 'Kamala Karki', due: 420 },
-  { name: 'Sher Bahadur Rai', due: 0 },
-  { name: 'Nirmala Limbu', due: 1980 },
-].map(c => ({
-  ...c,
-  image: 'https://via.placeholder.com/150/f1f5f9/64748b?text=' + encodeURIComponent(c.name.charAt(0).toUpperCase()),
-}));
+  { name: 'Ram Bahadur Thapa', due: 1250, image: FACES.manBlackCrew },
+  { name: 'Sita Sharma', due: 0, image: FACES.womanSmiling },
+  { name: 'Hari Prasad Adhikari', due: 340, image: FACES.manTurban },
+  { name: 'Gita Gurung', due: 0, image: FACES.womanBlackCrew },
+  { name: 'Krishna Yadav', due: 2100, image: FACES.manBlueRedShirt },
+  { name: 'Laxmi Magar', due: 150, image: FACES.womanSmiling },
+  { name: 'Dilli Ram Pokharel', due: 0, image: FACES.manBlackCrew2 },
+  { name: 'Mina Tamang', due: 875, image: FACES.womanBlackCrew },
+  { name: 'Bishnu Shrestha', due: 0, image: FACES.manHenley },
+  { name: 'Kamala Karki', due: 420, image: FACES.womanSmiling },
+];
